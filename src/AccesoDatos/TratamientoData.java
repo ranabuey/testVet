a2f53a8c7d82d606b048cd2761061b0584f016d1
ranabuey <ranabuey@gmail.com>
2023-10-06 13:11:38 -0300

    tratamiento data

--- src/AccesoDatos/TratamientoData.java
+++ src/AccesoDatos/TratamientoData.java
@@ -18,46 +18,47 @@ import javax.swing.JOptionPane;
 public class TratamientoData {
 
     private Connection con = null;
-    
+
     public TratamientoData() {
-        
+
         con = Conexion.getConexion();
-        
+
     }
-    
-    public void guardarTratmiento(Tratamiento trat){
-        
+
+    public void guardarTratmiento(Tratamiento trat) {
+
         String sql = "INSERT INTO tratamiento ( descripcion, medicamento, importe, tipoTratamiento, activo) VALUES (?,?,?,?,?)";
-    try{
-        PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-        ps.setString(1,trat.getDescripcion());
-        ps.setString(2,trat.getMedicamento());
-        ps.setString(3, trat.getTipoTratamiento());
-        ps.setDouble(4,trat.getImporte());
-        ps.setBoolean(5,trat.isActivo());
-        ps.executeUpdate();
-        ResultSet rs = ps.getGeneratedKeys();
-        if(rs.next()){
-            trat.setIdTratamiento(rs.getInt(1));
-            JOptionPane.showMessageDialog(null, "Tratamiento añadida con exito");
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, trat.getDescripcion());
+            ps.setString(2, trat.getMedicamento());
+            ps.setString(3, trat.getTipoTratamiento());
+            ps.setDouble(4, trat.getImporte());
+            ps.setBoolean(5, trat.isActivo());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                trat.setIdTratamiento(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Tratamiento añadida con exito");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error al acceder a la tabla Materia " + ex.getMessage());
         }
-    }catch(SQLException ex){
-         JOptionPane.showMessageDialog(null, "error al acceder a la tabla Materia " + ex.getMessage());
     }
-    }
-    public void modificarTratamiento(Tratamiento trat){
+
+    public void modificarTratamiento(Tratamiento trat) {
         String sql = "UPDATE tratamiento SET descripcion=?,medicamento=?, TipoTratamiento=?, importe=?, activo=? ";
         PreparedStatement ps = null;
-        try{
-        ps = con.prepareStatement(sql);
-        ps.setString(1,trat.getDescripcion());
-        ps.setString(2,trat.getMedicamento());
-        ps.setString(3, trat.getTipoTratamiento());
-        ps.setDouble(4,trat.getImporte());
-        ps.setBoolean(5,trat.isActivo());
-        int exito = ps.executeUpdate();
-        ResultSet rs = ps.getGeneratedKeys();
-         if (exito == 1) {
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, trat.getDescripcion());
+            ps.setString(2, trat.getMedicamento());
+            ps.setString(3, trat.getTipoTratamiento());
+            ps.setDouble(4, trat.getImporte());
+            ps.setBoolean(5, trat.isActivo());
+            int exito = ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (exito == 1) {
                 JOptionPane.showMessageDialog(null, "Modificada exitosamente");
             } else {
                 JOptionPane.showMessageDialog(null, "el tratamiento no existe");
@@ -68,30 +69,32 @@ public class TratamientoData {
 
         }
     }
-    public void eliminarTratamiento(int id){
-        try{
+
+    public void eliminarTratamiento(int id) {
+        try {
             String sql = "UPDATE tratamiento SET activo=0 WHERE idTrtamiento=?";
             PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             int fila = ps.executeUpdate();
-             if(fila ==1 ){
-                  JOptionPane.showMessageDialog(null, "Se elimino el tratamiento");
-             }
+            if (fila == 1) {
+                JOptionPane.showMessageDialog(null, "Se elimino el tratamiento");
+            }
             ps.close();
-    
-        }catch (SQLException ex) {
+
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla tratamiento" + ex.getMessage());
 
         }
 
     }
+
     public List<Tratamiento> listarTratamiento() {
         List<Tratamiento> tratamiento = new ArrayList<>();
-        try{
-            String sql = "SELECT * FROM tratamiento WHERE activo =1"; 
+        try {
+            String sql = "SELECT * FROM tratamiento WHERE activo =1";
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 Tratamiento tr = new Tratamiento();
                 tr.setIdTratamiento(rs.getInt("idTratamiento"));
                 tr.setDescripcion(rs.getString("Descripcion"));
@@ -102,6 +105,35 @@ public class TratamientoData {
         } catch (SQLException ex) {
             Logger.getLogger(TratamientoData.class.getName()).log(Level.SEVERE, null, ex);
         }
-      return tratamiento;
+        return tratamiento;
     }
-}
+
+    public List<Mascota> obtenerMascotasMismoTratamiento(String tratamiento) {                              // chekear metodo
+        List<Mascota> mascotasList = new ArrayList<>();
+        String sql = "SELECT m.idMascota, alias, especie, raza FROM visita v, mascota m, tratamiento t WHERE v.idMascota=m.idMascota AND v.idTratamiento=t.idTratamiento AND t.tipoTratamiento=? AND m.activo=1";
+        
+        try {
+            PreparedStatement ps;
+            ps = con.prepareStatement(sql);
+            ps.setString(1, tratamiento);
+            ResultSet rs = ps.executeQuery();
+           
+            while (rs.next()) {
+                Mascota masc = new Mascota();
+                masc.setIdMascota(rs.getInt("idMascota"));
+                masc.setAlias(rs.getString("alias"));
+                masc.setEspecie(rs.getString("especie"));
+                masc.setRaza(rs.getString("raza"));
+                
+                mascotasList.add(masc);
+            }
+            ps.close();
+        
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se obtuvo la tabla MASCOTA : " + ex.getMessage());
+        }
+        return mascotasList;
+
+        }
+
+    }
